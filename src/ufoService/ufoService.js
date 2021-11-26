386aee6680eb0c4e963874ffe182c02e33538146
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-11-26 18:04:50 +0100

    feat: added price, journey and amenities properties and getters

--- src/ufoService/ufoService.js
+++ src/ufoService/ufoService.js
@@ -1,20 +1,39 @@
 let ufoServicePrototype = {
-    init: function(name, description, category){
+    init: function(name, description, price, journey, category, amenities){
         this.name = name;
         this.description = description;
+        this.price = price;
+        this.journey = journey;
         this.category = category;
-
+        this.amenities = amenities;
+        
         return this;
     },
+
+    // Getters
     getName: function() {
         return("I'am " + this.name);
     },
     getDescription: function() {
-        return(this.description);
+        return this.description;
+    },
+    getJourney: function() {
+        return this.journey;
     },
     getCategory: function() {
         return this.category;
+    },
+    getPrice: function() {
+        return this.price;
+    },
+    getAmenities: function() {
+        return this.amenities;
+    },
+
+    calculatePrice() {
+
     }
+    
 };
 
 module.exports = ufoServicePrototype;

172c6ae7cd7a93df00ae13b11a177eb899a442f7
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-11-28 16:51:33 +0100

    feat: added isDiscountMonth method to serviceCategory prototype

--- src/serviceCategory/serviceCategory.js
+++ src/serviceCategory/serviceCategory.js
@@ -8,6 +8,14 @@ let serviceCategoryPrototype = {
     getName: function () {
         return this.name;
     },
+    isDiscountMonth: function(date = Date.now()) {
+        // Los meses en las fechas de JavaScript van de 0 a 11.
+        // Parametro default para poder testear pasando valores fijos.
+        // Metodo que duelve true o false dependiendo si no encontramos en diciembre o no.
+        let actualDate = new Date(date);
+        let actualMonth = actualDate.getMonth();
+        return actualMonth == 11 ? true : false;
+    },
     calculateDiscount: function () {
         return "This " + this.getName() + 
                 " service category offers " + 

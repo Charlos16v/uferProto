4f2c4b4efc27a5f989cec8886f3c87cfd81e2663
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-12-02 00:28:52 +0100

    test: added multiple test run on calculatePrice() up to 1000 retrys

--- src/ufoService/test/ufoService.test.js
+++ src/ufoService/test/ufoService.test.js
@@ -12,11 +12,12 @@ describe('scope tests serviceCategory prototype', () => {
 
 
     // SETUP
-    beforeAll( () => {
+    beforeEach( () => {
         journey = journeyFactory.init("MurciaGalaxy", "MarbellaFresh", 1000)
         category = premiumCategory.init();
 
-        ufer = uferService.init("Gold", "fresh", journey, category, []);
+        ufer = uferService.init("Gold", "fresh", journey, []);
+        ufer.setCategory(category);
     });
     // BASIC TEST
     test('init() from uferServicePrototype', () => {
@@ -36,30 +37,53 @@ describe('scope tests serviceCategory prototype', () => {
     });
 
     test('calculatePrice() from uferServicePrototype', () => {
-        
+        // Al estar la logica usando numeros aleatorios por probabilidad
+        // he decidido testear multiples veces para verificar que realmente
+        // se estan quedando entre los rangos de valores deseados los resultados.
+        // Ya que con testear 1 vez no quedaria completo, repetimos el test 10000 veces.
+
+        const NRUNS = 10000;
+
         // CASO SE APLICA DESCUENTO CONCRETAMENTE DE PREMIUM CATEGORY.
         let dateWithDiscount = new Date(2021, 11, 15);
         Date.now = jest.fn(() => dateWithDiscount);
+
+        for (let i = 0; i < NRUNS; i++) {
         
-        ufer.calculatePrice();
+            ufer.calculatePrice();
         
-        // En este caso al ser categoria Premium y estar en 
-        // diciembre se deberia haber aplicado entre el 10% y el 40%
-        // de descuento sobre el precio del servicio
-        // (440 con el 40% y 670 con el 10% aplicados).
-        // Por lo que nuestro margen de resultados validos esta entre 440 y 670.
+            // En este caso al ser categoria Premium y estar en 
+            // diciembre se deberia haber aplicado entre el 10% y el 40%
+            // de descuento sobre el precio del servicio
+            // (440 con el 40% y 670 con el 10% aplicados).
+            // Por lo que nuestro margen de resultados validos esta entre 440 y 670.
+            console.log(i);
+            /* Usado anteriormente tanto para revisar valores 
+            obtenidos como ver el valor actual de la iteracion en el bucle.
+            //console.log(ufer.getPrice());
+            //console.log(i);
+            */
+            expect(ufer.getPrice()).toBeGreaterThanOrEqual(4224); 
+            expect(ufer.getPrice()).toBeLessThanOrEqual(6336);
+        };
+    
 
 
-        expect(ufer.getPrice()).toBeGreaterThanOrEqual(440); 
-        expect(ufer.getPrice()).toBeGreaterThanOrEqual(670);
-
-        
         // CASO SE NO APLICA DESCUENTO AL NO SER DICIEMBRE.
         let dateWithoutDiscount = new Date(2021, 4, 31);
         Date.now = jest.fn(() => dateWithoutDiscount);
         
         ufer.calculatePrice();
         expect(ufer.getPrice()).toBe(7040);
-    });
 
+
+        // CASO SE NO APLICA DESCUENTO AL NO TENER CATEGORIA.
+        
+        Date.now = jest.fn(() => dateWithDiscount);
+        // Setteamos la propiedad category de ufoService a null.
+        ufer.category = null;
+
+        ufer.calculatePrice();
+        expect(ufer.getPrice()).toBe(7040);
+    });
 })

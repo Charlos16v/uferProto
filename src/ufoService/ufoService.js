a95e1b9f1519557c3c0a558258c7652b7851e4fa
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-12-02 02:02:53 +0100

    feat: prepareDiscount closure in ufoService and implemented more business logic

--- src/ufoService/ufoService.js
+++ src/ufoService/ufoService.js
@@ -1,12 +1,17 @@
 let ufoServicePrototype = {
-    init: function(name, description, journey, category, amenities){
+    init: function(name, description, journey, amenities){
         this.name = name;
         this.description = description;
         this.journey = journey;
-        this.category = category;
+
+        // La categoria la setteamos con el setter.
+        //this.category = category;
         this.amenities = amenities;
         this.price;
         this.discountByCategory;
+        //? PENDIENTE DE IMPLEMENTAR SISTEMA DE RESERVAS?
+        //?this.reserved = false;
+        // COSTE BASE QUE SE COBRA SI O SI EN EL SERVICIO.
         this.KEYBASECOST = 40;
 
         //! MOVER A CATEGORY??
@@ -40,41 +45,72 @@ let ufoServicePrototype = {
     getKEYBASECOST: function() {
         return this.KEYBASECOST;
     },
+    getDistance: function(){
+        return this.getJourney()?.getDistance();
+    },
 
     //! Setters necesarios??
     setPrice: function(price) {
         this.price = price;
     },
+    setCategory: function(category) {
+        this.category = category;
+        this.prepareDiscount(this.category);
+    },
+    /* PENDIENTE IMPLEMENTAR?
+    setReserved: function(value) {
+        this.reserved = value;
+    },
+
+    isReserved: function() {
+        return this.reserved == true;
+    },*/
 
     // Business logic.
-    getDiscount: function(){
-        // Obtenemos el valor del porcentaje desde el proto categoria.
-        let discountPercentage = this.getCategory()?.getDiscount();
-        
-        // Setteamos el valor en la propiedad discountByCategory.
-        this.discountByCategory = discountPercentage;
+    // CLOSURE.
+    prepareDiscount: function(category){
+        let putDiscount = function() {
+            function applyDiscount() {
+                // Comprobamos antes que el ufoService tenga asignada una category.
 
-        // Devolvemos el valor ya que va a ser usado posteriormente en calculatePrice()
-        // pero tambien nos interesa que el valor sea persistido en el objeto del servicio.
-        return this.discountByCategory;
-    },
-    getDistance: function(){
-        return this.getJourney()?.getDistance();
+                // Setteamos el valor en la propiedad discountPercentatge de category
+                this.discountPercentage = category.getDiscount();
+                return category.getDiscount();
+            }
+            return applyDiscount;
+        };
+        Object.defineProperty(category, 'applyDiscount', {
+            value: putDiscount(),
+            writable: true
+        });
+        // Aqui se lo estaria añadiendo a ufoService.
+        // this.applyDiscount = putDiscount();
     },
     calculatePrice: function() {
         let distance = this.getDistance();
-        let discountPercentage = this.getDiscount();
+
+        let discountPercentage = this.getCategory()?.applyDiscount()
 
         // Precio sin descontar descuento.
         var price = this.getKEYBASECOST() + (distance * this.getKEYMETERPRICE());
         
-        if (discountPercentage < 0) {
+        if (discountPercentage > 0) {
             // Si hay descuento se calcula y se le resta al precio.
-            let discountQuantity = this.getCategory?.getQuantityToDiscount();
+            let discountQuantity = this.getCategory()?.getQuantityToDiscount(price, discountPercentage);
             this.setPrice(price - discountQuantity);
+        } else {
+            this.setPrice(price);
         }
-        this.setPrice(price);
-    }
+        
+    },
+
+    /* PENDIENTE IMPLEMENTAR?
+    reserveUfoService: function() {
+        if (this.isReserved() == false){
+            this.setReserved(true);
+            this.prepareDiscount();
+        }
+    }*/
     
 };
 

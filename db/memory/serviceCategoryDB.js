9010b7b158f4d6b22ca996ca0d85b450d13360a5
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-01-02 13:25:30 +0100

    feat: serviceCategoryDB in memory database

---
+++ db/memory/serviceCategoryDB.js
@@ -0,0 +1,22 @@
+const serviceCategoryDB = [
+    {
+        name: "Standard",
+        minDiscount: 10,
+        maxDiscount: 7,
+        KEYMETERPRICE: 8
+    },
+    {
+        name: "Comfort",
+        minDiscount: 15,
+        maxDiscount: 25,
+        KEYMETERPRICE: 12
+    },
+    {
+        name: "Premium",
+        minDiscount: 10,
+        maxDiscount: 40,
+        KEYMETERPRICE: 20
+    }
+]
+
+module.exports = serviceCategoryDB;

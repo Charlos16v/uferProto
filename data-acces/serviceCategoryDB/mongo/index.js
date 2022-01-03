2ae425498b187380c81b9f89ce3057129ebff75c
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-01-03 21:39:45 +0100

    feat: implemented crud of data-acces layer with mongodb

--- data-acces/serviceCategoryDB/mongo/index.js
+++ data-acces/serviceCategoryDB/mongo/index.js
@@ -3,30 +3,56 @@ let serviceCategoryProto = require('../../../domain/serviceCategory/serviceCateg
 let serialize = require('./serializer.js');
 
 const serviceCategoryDataAcces = (function serviceCategoryMongoDB() {
-    
+
     const getAll = () => {
         return ServiceCategory.find({})
-        .then(serialize)
+            .then(serialize)
     };
 
-    const findByProperty = (prop, val) => {
+    const findByProperty = async (prop, val) => {
+        if (prop === 'id') {
+            prop = '_id'
+        }
+        let filter = {[prop]: val}
+        const query = ServiceCategory.findOne(filter);
+        const doc = await query.exec()
         
+        return serialize(doc);
     };
 
     const add = (categoryInfo) => {
-        
+        let serviceCategory = serviceCategoryProto.init(categoryInfo.name, categoryInfo.minDiscount, categoryInfo.maxDiscount, categoryInfo.KEYMETERPRICE);
+        let newServiceCategory = {
+            name: serviceCategory.name,
+            minDiscount: serviceCategory.minDiscount,
+            maxDiscount: serviceCategory.maxDiscount,
+            KEYMETERPRICE: serviceCategory.KEYMETERPRICE
+        };
+        return ServiceCategory.create(newServiceCategory)
+            .then(serialize)
     };
 
     const deleteById = (id) => {
-
+        return ServiceCategory.findByIdAndDelete(id)
+            .then(resp => {
+                return {
+                    id: resp._id.toString(),
+                    status: 'success'
+                }
+            })
+            .catch(err => {
+                return {
+                    status: 'fail'
+                }
+            })
     };
 
-return {
-    getAll,
-    findByProperty,
-    add,
-    deleteById
-};
+    return {
+        getAll,
+        findByProperty,
+        add,
+        deleteById
+    };
 
 })();
 

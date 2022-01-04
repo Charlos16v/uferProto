4b711921c02e14a9ceecddbb9d186929db871d2e
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-01-04 15:08:27 +0100

    feat: added journeyController to routes

--- drivers/webserver/routes/index.js
+++ drivers/webserver/routes/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const serviceCategoryController = require('../../../controllers/serviceCategoryController');
+const journeyController = require('../../../controllers/journeyController');
 
+// serviceCategory
 router
     .get('/serviceCategory', serviceCategoryController.getAll)
     .get('/serviceCategory/:prop/:value', serviceCategoryController.findByProperty)
     .put('/serviceCategory', serviceCategoryController.add)
-    .delete('/serviceCategory/:id', serviceCategoryController.deleteById)
+    .delete('/serviceCategory/:id', serviceCategoryController.deleteById);
+
+// journey
+router
+.get('/journey', journeyController.getAll)
+.get('/journey/:prop/:value', journeyController.findByProperty)
+.put('/journey', journeyController.add)
+.delete('/journey/:id', journeyController.deleteById)
+
 
 module.exports = router;

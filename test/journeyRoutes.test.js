4a782d1916c2a83b8ac90e87c1931bd75adb1547
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-01-12 17:53:40 +0100

    test: created and implemented crud integration tests for journey entity

--- test/journeyRoutes.test.js
+++ test/journeyRoutes.test.js
@@ -32,4 +32,59 @@ const app = require('../drivers/webserver/server.js');
          expect(res.body[0]._id).not.toBeFalsy();
          expect(res.body[0].startPoint).not.toBeFalsy();
      });
+
+     test("Test findByProperty /journey", async () => {
+        const res = await request(app)
+            .get(`/journey/startPoint/MurciaGalaxy`);
+        expect(res.get('Content-Type')).toEqual(expect.stringMatching('/json'));
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('_id', 'startPoint', 'endPoint', 'distance');
+        
+        expect(res.body._id).not.toBeFalsy();
+        expect(res.body._id).toBe('61b0f513646886f408bd0730');
+        
+        expect(res.body.startPoint).not.toBeFalsy();
+        expect(res.body.startPoint).toBe('MurciaGalaxy');
+
+        expect(res.body.endPoint).not.toBeFalsy();
+        expect(res.body.endPoint).toBe('MarbellaFresh');
+
+        expect(res.body.distance).not.toBeFalsy();
+        expect(res.body.distance).toBe(1000);
+    });
+
+    test("Test add and delete /journey", async () => {
+        let body = {
+            "startPoint": "maquinaria",
+            "endPoint": "master",
+            "distance": 1
+        };
+
+        const resAdd = await request(app)
+            .put(`/journey`)
+            .send(body);
+
+        expect(resAdd.get('Content-Type')).toEqual(expect.stringMatching('/json'));
+        expect(resAdd.statusCode).toEqual(200);
+
+        expect(resAdd.body._id).not.toBeFalsy();
+        
+        expect(resAdd.body.startPoint).not.toBeFalsy();
+        expect(resAdd.body.startPoint).toBe('maquinaria');
+
+        expect(resAdd.body.endPoint).not.toBeFalsy();
+        expect(resAdd.body.endPoint).toBe('master');
+
+        expect(resAdd.body.distance).not.toBeFalsy();
+        expect(resAdd.body.distance).toBe(1);
+
+        // Sacamos el id del journey creado para borrarlo.
+        let id = resAdd.body._id;
+
+        const resDel = await request(app)
+            .delete(`/journey/${id}`);
+
+        expect(resDel.get('Content-Type')).toEqual(expect.stringMatching('/json'));
+        expect(resDel.statusCode).toEqual(200);
+    });
  });

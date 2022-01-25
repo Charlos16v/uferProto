3ff52cbd9b7f5b6d9a80e90a2d1819fce92d7e01
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2022-01-25 14:45:40 +0100

    feat: implemented --del arg for delete by id

--- drivers/cli/index.js
+++ drivers/cli/index.js
@@ -3,15 +3,21 @@ const serviceCategoryDB = require('../../data-access/serviceCategoryDB/index.js'
 
 let printHelp = function () {
     console.log(`
+    ServiceCategory CLI UI!
+    
+    ==============================================
     Help usage:
     --index        list service categories.
     --show {name}  find serviceCategory by name.
+    --del {id}     delete serviceCategory by id.
     --help         print help.
+    ==============================================
   `);
 }
 
-let valid = args.index || args.show
+let valid = args.index || args.show || args.del
 
+// NODE_ENV=dev node drivers/cli/index.js --help
 if (args.help || !valid) {
     printHelp()
     process.exit(1)
@@ -28,6 +34,14 @@ if (args.index) {
 // NODE_ENV=dev node drivers/cli/index.js --show 'Premium'
 if (args.show) {
     serviceCategoryDB.findByProperty('name', args.show).then(data => {
+        console.log(data);
+        process.exit(1)
+    })
+}
+
+// NODE_ENV=dev node drivers/cli/index.js --del 3 
+if (args.del) {
+    serviceCategoryDB.deleteById(args.del).then(data => {
         console.log(data);
         process.exit(1)
     })
